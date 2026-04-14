e87165d2a4954e8454d6532787193281557a8a63
Pradeep-kumar079 <pradeepk9348@gmail.com>
2026-04-14 12:51:38 +0530

    images error fixed

--- client/src/Components/Post.jsx
+++ client/src/Components/Post.jsx
@@ -18,7 +18,7 @@ const Post = () => {
   const handleChange = (e) => {
     const { name, value, files } = e.target;
 
-    if (files) {
+    if (files && files[0]) {
       setFormData({ ...formData, [name]: files[0] });
       setPreview(URL.createObjectURL(files[0]));
     } else {
