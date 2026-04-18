3b5743a355680c4905e7eabce6309e6cb3b3ee80
Pradeep-kumar079 <pradeepk9348@gmail.com>
2026-04-19 01:28:27 +0530

    fix api folder location inside src

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,8 +1,20 @@
 import API from "../api";
 
 const api = API.create({
-  baseURL: "/api",
-  withCredentials: true,
+  baseURL: "https://pradeepkumar.site/api",
+});
+
+// 🔥 FORCE TOKEN ATTACH
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  } else {
+    delete config.headers.Authorization; // 🔥 VERY IMPORTANT
+  }
+
+  return config;
 });
 
 export default api;
